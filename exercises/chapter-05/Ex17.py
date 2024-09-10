3a879b5f2b4b23bb8fc3161653c57a4607b2a3c0
kaioln <kaio.leonardoss@gmail.com>
2024-09-10 15:40:49 -0300

    Y: new exercises and correction needed

---
+++ exercises/chapter-05/Ex17.py
@@ -0,0 +1,30 @@
+value = int(input("Enter the amount to pay: "))
+banknotes = 0
+current = 100
+remove = value
+
+if value == 0:
+    print("You don't have money to pay!")
+else:
+    while True:
+        if current <= remove:
+                remove -= current
+                banknotes += 1
+        else:
+            if banknotes > 0:
+                print(f"{banknotes} banknotes of ${current}")
+            if remove == 0:
+                print("Your money is gone")
+                break
+            elif current == 100:
+                current = 50
+            elif current == 50:
+                current = 20
+            elif current == 20:
+                current = 10
+            elif current == 10:
+                current = 5
+            elif current == 5:
+                current = 1
+            banknotes = 0
+            
