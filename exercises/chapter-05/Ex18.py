765c12c3878360ae235c41a801c532bd70bdc65b
kaioln <kaio.leonardoss@gmail.com>
2024-09-11 14:46:49 -0300

    Z: new exercise

--- exercises/chapter-05/Ex18.py
+++ exercises/chapter-05/Ex18.py
@@ -1,30 +1,56 @@
-value = int(input("Enter the amount to pay: "))
+value = float(input("Enter the amount to pay: "))
 banknotes = 0
-current = 100
+coins = 0
+currentBanknotes = 100
+currentCoins = 50
 remove = value
+removeCents = int(value * 100 + 0.5)
 
-if value == 0:
-    print("You don't have money to pay!")
-else:
-    while True:
-        if current <= remove:
-                remove -= current
-                banknotes += 1
-        else:
-            if banknotes > 0:
-                print(f"{banknotes} banknotes of ${current}")
-            if remove == 0:
-                print("Your money is gone")
-                break
-            elif current == 100:
-                current = 50
-            elif current == 50:
-                current = 20
-            elif current == 20:
-                current = 10
-            elif current == 10:
-                current = 5
-            elif current == 5:
-                current = 1
-            banknotes = 0
-            
+
+print("Banknotes:")
+while currentBanknotes > 0:
+    count = 0
+    while removeCents >= currentBanknotes * 100:
+        removeCents -= currentBanknotes * 100
+        count += 1
+    if count > 0:
+        print(f"{count} banknotes of ${currentBanknotes}")
+    if remove == 0:
+        print("Your money is gone")
+        break
+    if count > 0:
+        print(f"{count} banknote(s) of ${banknotes}")
+    elif currentBanknotes == 100:
+        currentBanknotes = 50
+    elif currentBanknotes == 50:
+        currentBanknotes = 20
+    elif currentBanknotes == 20:
+        currentBanknotes = 10
+    elif currentBanknotes == 10:
+        currentBanknotes = 5
+    elif currentBanknotes == 5:
+        currentBanknotes = 1
+        break
+    banknotes = 0
+
+print("Coins:")
+while currentCoins > 0:
+    count = 0
+    while removeCents >= currentCoins:
+        removeCents -= currentCoins
+        count += 1
+    if count > 0:
+        print(f"{count} coin(s) of ${currentCoins / 100:.2f}")
+    elif currentCoins == 100:
+        currentCoins = 50
+    elif currentCoins == 50:
+        currentCoins = 20
+    elif currentCoins == 20:
+        currentCoins = 10
+    elif currentCoins == 10:
+        currentCoins = 5
+    elif currentCoins == 5:
+        currentCoins = 1
+        break
+    coins = 0
+    
